18caebdabd1ef4735e58236462d0af9d33311234
bradsta <bradstarsca@gmail.com>
2016-05-04 17:49:52 -0700

    Implemented Lumberjack
    Put in a lumberjack dude, also changed the sky to a solid color thanks 2
    Hatef.

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+    public int xPerSecond = 1;
+
+    private Vector3 translation;
+
+    void Start ()
+    {
+        this.translation = new Vector3(0, 0, this.xPerSecond);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        this.transform.Translate(translation * Time.deltaTime);
+	}
+
+    void OnCollisionEnter (Collision col)
+    {
+        if (col.gameObject.CompareTag("Tree"))
+        {
+            Destroy(col.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+
+}
