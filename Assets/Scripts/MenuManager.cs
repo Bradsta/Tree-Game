22f5ccc993f1a7a23d0a21682fc528e70eb52242
Hatef Kasraei <hatefkasraei@gmail.com>
2016-05-25 00:19:41 -0700

    added 3 menu buttons, added menu scene, added some functionalities to the buttons

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+
+public class MenuManager : MonoBehaviour 
+{
+	public void LoadSenceAndKeepCurrentScene(string sceneName)
+	{
+		SceneManager.LoadScene (sceneName);
+	}
+
+	public void LoadSceneAndUnloadCurrentScene(string sceneName)
+	{
+		SceneManager.UnloadScene (SceneManager.GetActiveScene().name);
+		SceneManager.LoadScene (sceneName);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit ();
+	}
+
+
+}
